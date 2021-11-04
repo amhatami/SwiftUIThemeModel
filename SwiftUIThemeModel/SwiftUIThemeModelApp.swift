f341637991ae3e528a72e94fc0325f8e24981579
Amir Hatami <am.hatami@gmail.com>
2021-11-04 10:24:24 -0700

    Add files via upload

---
+++ SwiftUIThemeModel/SwiftUIThemeModelApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIThemeModelApp.swift
+//  SwiftUIThemeModel
+//
+//  Created by Pooya on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThemeModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+                .environmentObject(ThemeSource())
+        }
+    }
+}
